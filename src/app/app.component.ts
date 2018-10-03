1d9a56210056a89eae0eaa731092c3ac97c57cdd
Raghavendra Badveeti <raghavendra.badveeti@jda.com>
2018-10-03 11:34:15 +0530

    Included Angular DomSanitizer to fix XSS issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SecurityContext } from '@angular/core';
 import html2jsonLib from 'html2json';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,26 @@ import html2jsonLib from 'html2json';
 export class AppComponent {
 
   inputString: string =
-    '<ul><li><strong class="ql-size-large" style="color: rgb(0, 138, 0);"><em><s><u>Hello World</u></s></em></strong></li></ul><p><br></p><ul><li><a href="https://www.npmjs.com/package/ng-quill" target="_blank" class="ql-size-large">NG-Quill Test for Angular 1.x</a></li></ul>';
+    '<ul><li class="ql-indent-1 ql-align-center">Use<strong><em> Angular DOMSanitize</em></strong>r <s><u>to filter XSS</u></s> <strong style="color: rgb(230, 0, 0); background-color: rgb(255, 255, 0);">issues </strong></li></ul>';
 
   htmlGeneratedByRTE: string = '';
   jsonGeneratedByLibrary: string = '';
   htmlGeneratedByLibrary: string = '';
 
+  constructor(private sanitizer: DomSanitizer) {
+
+  }
+
   getUpdatedModelValueEvent(htmlString: any) {
     this.htmlGeneratedByRTE = htmlString;
     this.jsonGeneratedByLibrary = JSON.stringify(html2jsonLib.html2json(this.htmlGeneratedByRTE));
-    this.htmlGeneratedByLibrary = html2jsonLib.json2html(JSON.parse(this.jsonGeneratedByLibrary));
+    this.htmlGeneratedByLibrary = this.bypassSecurityTrustHtml(html2jsonLib.json2html(JSON.parse(this.jsonGeneratedByLibrary)));
 
+  }
+
+  bypassSecurityTrustHtml(htmlString: string | null): string | null {
+    return this.sanitizer.sanitize(SecurityContext.HTML, htmlString);
+    return htmlString;
   }
 
 }
